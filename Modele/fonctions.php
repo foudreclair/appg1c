95bef3534a83a89b0457ae9f6ab28600cd75e6e8
de Javel <aymeric@dejavel.fr>
2016-12-20 22:18:38 +0100

    Ajout de fonctions select et insert générique

--- Modele/fonctions.php
+++ Modele/fonctions.php
@@ -9,4 +9,37 @@ function select_user($user) {
 	$result->close();
 }
 
+
+function select($table,$quoi,$cond) {
+	include('connexion_bdd.php');
+	$selection ='';
+	foreach ($quoi as $key => $value) {
+		$selection .= $value;
+		$selection .= ',';
+	}
+	$selection = substr($selection, 0, -1);
+	$result = $mysqli->query('SELECT '.$selection .' FROM '.$table.' where '.$cond);
+	$donnees = $result ->fetch_array(MYSQLI_ASSOC);
+	return $donnees;
+	$result -> close();
+
+}
+
+function insert($table,$quoi,$valeurs) {
+	include('connexion_bdd.php');
+	$selection ='';
+	$va='';
+	foreach ($quoi as $key => $value) {
+		$selection .= $value;
+		$selection .= ',';
+		$va .= '?';
+		$va .= ','
+	}
+	$selection = substr($selection, 0, -1);
+	$va substr($va, 0, -1);
+	$req = $mysqli->prepare('INSERT INTO '.$table.'('.$selection.') VALUES('.$va.')');
+	$req -> execute($valeurs);
+
+}
+
 ?>
