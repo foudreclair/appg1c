20897449dd578b6859e24ef71d74692af9d3d275
aXteroid <ferreradrien@hotmail.fr>
2017-01-09 09:40:44 +0100

    #MODIFIER APPARTEMENT
    Voila j'ai ajouté l'arborescence (3-4 pages) qui permet d'ajouter un appartement, ajouter une pièce dans cet appartement, et ajouter un nombre relatif de capteurs dans cette pièce.

--- Vue/pagereglage.php
+++ Vue/pagereglage.php
@@ -1,3 +1,8 @@
+<?php
+require ('../Controleur/traitement.php');
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,19 +19,62 @@
 		</ul>
 	</nav>
 	<div id="full_bloc">
-		<div id="left_bloc">
-			<p>Surface Totale (en m²)  <input placeholder="20m²" type="number" step="1" method="post" name="surface"></p>
-			<p> Nombre de pièce <input placeholder="5" type="number" step="1" method="post" name="nombre_piece">
-			<p> Adresse 
-				<input type="test" name="adresse" placeholder="28 rue Notre Dame des Champs" method="post"> 
-				<input type="number" name="code_postal" placeholder="75006" method="post">
-				<input type="text" name="pays" placeholder="France" method="post">
+		<form class="left_bloc" method="post" action="../Controleur/traitement.php">
+			<h2> Ajouter un appartement</h2>
+			<p> Nom appartement : 
+				<input type="text" name="nom_appartement" id="nom_appartement">
+			</p>
+			<p>Type appartement
+				<select name="type_appartement" id="type_appartement">
+					<option value="primaire"> Primaire</option>
+					<option value="secondaire"> Secondaire </option>
+				</select>
+			</p>
+			<p> Rue
+				<input type="text" name="adresse_appartement" id="adresse_appartement"> 
+			</p>
+			<p> Ville
+				<input type="text" name="ville_appartement" id="ville_appartement">
+			</p>
+			<p> Pays de Résidence
+				<input type="text" name="pays_appartement" id="pays_appartement">
+				<input type="hidden" name="declencheur" id="declencheur" value="1">
 			</p>
 			<p> Nombre de personnes dans l'appartement
-				<input type="number" name="nombre_personne" step="1" method="post">
+				<input type="number" name="nombre_personne_appartement" id="nombre_personne_appartement" step="1">
 			</p>
-			<input type="button" value="Valider" name="valider_appart">
+			<input type="submit" value="Valider">
+		</form>
+		<div class="right_bloc">
+			
+			<form method="post" action="../Controleur/traitement.php">
+				<p>Nombre de capteurs : <input type="number" name="nombre_capteurs" id="nombre_capteurs" step="1" /></p>
+				<input type="submit" name="valider_nombre_capteurs">
+				<input type="hidden" name="declencheur" id="declencheur" value="3">
+			</form>
+			<form method="post" action="pagereglage.php">
+				<?php
+				for ($i=0; $i < "nombre_capteurs"; $i++) { 
+				 	echo '<input type="text" name="nom_capteur" id="nom_capteur"></br>
+				 	<select name="fonctionnalite" id="type_capteur">
+				 				<option value="temperature"> Température </option>
+				 				<option value="lumiere"> Lumière </option>
+				 				<option value="fumee"> Fumée </option>
+				 				<option value="camera"> Caméra </option>
+				 				<option value="co2"> co2 </option>
+				 				<option value="onde_em"> Ondes électromagnétiques </option>
+				 				<option value="humidite"> Humidité </option>
+				 				<option value="capteur_ouverture"> Capteur ouverture </option>
+				 				<option value="pluviometre"> Pluviomètre </option>
+				 	</select>';
+					}
+				?>
+				<input type="submit">
+			</form>
 
+				<input type="hidden" name="declencheur" value="2">
+				<input type="submit" name="valider_capteurs">
+			</form>
 		</div>
 	
 </body>
