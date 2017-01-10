1933726074f1a3d61060ae90304eb71e021a7b4c
Pierre Goffic <pierre.goffic@isep.fr>
2017-01-10 17:41:37 +0100

    Register|Boutton

--- Vue/connexion.php
+++ Vue/connexion.php
@@ -28,7 +28,7 @@ if(isset($_GET["page"])) {
 <form method ="post" action = "Controleur/connexion.php">
 <input type = "text" name ="mail" placeholder="Entrez votre email"><br>
 <input type ="password" name ="password" placeholder="Mot de passe"><br>
-<input class ="button" type = "submit" name ='connexion' value ='Se connecter'>
+<input type = "submit" name ='connexion' value ='Se connecter'>
 <a href="index.php?page=register"><input class ="button" name ='register' value ='Créer un compte'></a>
 </form>
 </div>
