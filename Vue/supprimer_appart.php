7440137337ebae9a5fabad4a88819ec0670f7c78
Amauzo <Amauzo@users.noreply.github.com>
2017-02-07 11:51:11 +0100

    suppression des commentaire

--- Vue/supprimer_appart.php
+++ Vue/supprimer_appart.php
@@ -2,10 +2,9 @@
 $titre = "Domisep | Supprimer un appartement";
 include 'gabarit.php';
 require ('Controleur/traitement.php');
-// include ("Controleur/connexion.php");
+
 $iduser = $_SESSION ['id'];
 ?>
-<!--/////////////////////////////////////////////////////////////////// -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,21 +12,12 @@ $iduser = $_SESSION ['id'];
 <link rel="stylesheet" href="stylereglage.css" type="text/css" />
 <meta charset="utf-8">
 </head>
-<!--/////////////////////////////////////////////////////////////////// -->
+
 <body>
 	<div class="module3">
 		<div class="module form-block">
-			<!--
-	<nav class ="menu_appartement">
-		<ul>
-			<li><a href="#">Ajouter un appartement/pièce</a></li>
-			<li><a href="#">Modifier un appartement</a></li>
-			<li><a href="#">Supprimer un appartement</a></li>
-		</ul>
-	</nav>
-/////////////////////////////////////////////////////////////////// -->
+
 			<h1>Choisissez l'appartement à supprimer :</h1>
-			<!--/////////////////////////////////////////////////////////////////// -->
 
 			<form method="post" action="Controleur/traitement.php">
 
