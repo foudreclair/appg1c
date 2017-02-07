87e5ab9d5288b76f41b00b27f07d3415a42fb6e4
Pierre Goffic <pierre.goffic@isep.fr>
2017-02-07 16:38:51 +0100

    Pub | Carousel

---
+++ Vue/pub.php
@@ -0,0 +1,46 @@
+<?php
+$titre = "Cocoricode | Bievenue";
+include 'gabarit.php';
+?>
+<script
+	src="https://cdn.jsdelivr.net/jcarousel/0.3.4/jquery.jcarousel.min.js"></script>
+<script type="text/javascript" src="Vue/jcarousel.basic.js"></script>
+<link rel="stylesheet" type="text/css" href="Vue/jcarousel.basic.css">
+
+ <div class="wrapper">
+            <div class="jcarousel-wrapper">
+                <div class="jcarousel">
+                    <ul>
+                        <li><img src="Vue/images/1.jpg" width="600" height="400" alt=""></li>
+                        <li><img src="Vue/images/2.jpg" width="600" height="400" alt=""></li>
+                        <li><img src="Vue/images/3.jpg" width="600" height="400" alt=""></li>
+
+                    </ul>
+                </div>
+
+                <p class="photo-credits">
+                </p>
+
+                <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+                <a href="#" class="jcarousel-control-next">&rsaquo;</a>
+                
+                <p class="jcarousel-pagination">
+                    
+                </p>
+            </div>
+        </div>
+<div style="
+text-align : center;
+">
+<h2>
+<font color="white">
+<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+DomISEP ce n&#39;est pas l&#39;avenir, c&#39;est votre pr&egrave;sent.<br />
+Domisep, c&#39;est la plateforme qui rend votre maison autonome.<br />
+Domisep, c&#39;est la plateforme qui vous fait gagner du temps, de l&#39;argent, en respectant l&#39;environnement.<br />
+</font>
+</h2>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/icVVJ1ev7kE" frameborder="0" allowfullscreen></iframe>
+
+</div>
