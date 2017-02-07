0a7298863eac16eade0d192a5c6656743f8cba1c
Amauzo <Amauzo@users.noreply.github.com>
2017-02-07 12:14:46 +0100

    mail

--- Vue/contact.php
+++ Vue/contact.php
@@ -1,23 +1,38 @@
 <?php
-$titre = "Domisep | Contact";
+$titre = "Domicile | Contact";
 include 'gabarit.php';
+if (isset ( $_POST ["email"] ) and isset ( $_POST ["name"] ) and isset ( $_POST ["objet"] )) {
+	include ('Controleur/send_mail.php');
+	$email = htmlspecialchars($_POST ["email"]);
+	$name = htmlspecialchars($_POST ["name"]);
+	$objet = htmlspecialchars($_POST ["objet"]);
+	$message = htmlspecialchars($_POST ["body"]);
+	sendMail($_POST ["email"], $_POST ["objet"], $_POST ["body"]);
+	$text = "Votre mail a bien ete envoye. Nous vous contacterons prochainement !";
+}
 ?>
 <div class="module3">
 	<div class="form-block">
 		<div id="info_client">
+			<?php
+			if(isset($text)) {
+				echo '<font color="green">' . $text . '</font>';
+			}
+			?>
 			<p>Adresse :XX rue XXXXXX</p>
 			<p>Horraire : Lundi à Vendredi de 8h à 17h</p>
 			<p>Telephone : 01 XX XX XX XX</p>
 		</div>
 		<div id="champ_saisie">
 
-			<form method="post" action=".../Controleur/envoie_demail.php">
+			<form method="post" action="index.php?page=contact">
 				<input type="email" name="email" placeholder="Entrer votre email"></br>
 				<input type="text" name="name" placeholder="Entrez votre nom"></br>
 				<input type="text" name="objet"
 					placeholder="Entrez l'object du message"></br>
-				<textarea name="comment" name="message_complet"
-					placeholder="Entrez votre message" style="width:100%; height:100px;"></textarea>
+				<input name="body" id="body"
+					placeholder="Entrez votre message"
+					style="width: 100%; height: 100px;"></input>
 				</br>
 				<button class="button" type="submit" name="envoyer">Envoyer</button>
 			</form>
