7591d814f3554ec3936df2ac1a1ad9ec908da1f2
Landou75 <clement.landouar@isep.fr>
2017-02-07 11:45:56 +0100

    Footer orth

--- Vue/footer.php
+++ Vue/footer.php
@@ -2,7 +2,7 @@
 <div class="footer">
   <ul>
     <li>Mention légale</li>
-    <li>Réaliser par © Cocoricode 2017</li>
+    <li>Réalisé par © Cocoricode 2017</li>
     <li><a href="#contenu">Haut de page</a></li>
   </ul>
 </div>
