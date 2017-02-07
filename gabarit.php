980905b566c1b980ab1a2f94cc7c0ec3bf707d81
Pierre Goffic <pierre.goffic@isep.fr>
2017-02-07 13:07:43 +0100

    Revert "Add files via upload"
    This reverts commit 08960b89ead2e945457a97ea6512c6dacf2f985c.

--- gabarit.php
+++
@@ -1,7 +0,0 @@
-<?php
-//On inclut le haut de page
-include('Vue/header.php');
-?>
-<!--On affiche le contenu souhaité -->
-<div id="contenu">
-</div>
--- varscenar.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$option = '';
-foreach ($_POST['options'] as $key => $value) {
-	$value;
-}
-
-?>
