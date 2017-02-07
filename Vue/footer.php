978e090168455e69d1d32e4967c4a383fedc9820
Amauzo <Amauzo@users.noreply.github.com>
2017-02-07 11:09:50 +0100

    modification footer

--- Vue/footer.php
+++ Vue/footer.php
@@ -1,31 +1,10 @@
-<?php
-//IDEE
-//Faire un mini plan du site ?
-//Intégrer les liens ... fb etc et flux RSS
-//Insérer des Widgets
-//un espace de PUB
-//droit légal et copyright
-//lien vers le haut de page
-//A propos qui pointe vers un page static qui blablat sur l'entreprise du client
-
-
-//Lien vers le haut de page
-?>
 
 <div class="footer">
-  <div class="footer_2">
-    <ul>
-      <li <?php //activepage(XXX) ?>><a href="#">Plan du site</a></li>
-      <li <?php //activepage(XXX) ?>><a href="#">XXX</a></li>
-      <li <?php //activepage(XXX) ?>><a href="#contenu">Haut de page</a></li>
-    </ul>
-  </div>
-  <div class="mention_legal">
   <ul>
     <li>Mention légale</li>
     <li>Réaliser par © Cocoricode 2017</li>
+    <li><a href="#contenu">Haut de page</a></li>
   </ul>
-  </div>
 </div>
 </body>
 </html>
